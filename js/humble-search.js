aa3698a0177f889c2822e0418ed9e01cdf9cf1b9
tomnatt <github@tomnatt.com>
2024-12-03 16:30:10 +0000

    Properly horrible implementation of table search

--- js/humble-search.js
+++ js/humble-search.js
@@ -1,23 +1,54 @@
 var games_urls = ["https://raw.githubusercontent.com/tomnatt/humble-choice/refs/heads/main/output/json/humble-choice-2024.json"];
+var url = "https://raw.githubusercontent.com/tomnatt/humble-choice/refs/heads/main/output/json/humble-choice-2024.json";
 
 $(function() {
 
   // Remove dummy line
   $("#beforejs").remove();
 
-  // Get the games and populate the table
-  $.each(games_urls, function(i, url) {
-    $.getJSON(url, function(games) {
-      $.each(games, function(i, game) {
-        console.log(game);
+  // $.when(
 
-        gameName = "<td>" + game.name + "</td>";
-        gameBundle = "<td>" + game.month + " " + game.year + "</td>";
-        gameId = "<td>" + game.steam_id + "</td>";
+  //   // Get the games and populate the table
+  //   $.each(games_urls, function(i, url) {
+      $.getJSON(url, function(games) {
+        $.each(games, function(i, game) {
 
-        $("#games tbody").append("<tr>" + gameName + gameBundle + gameId + "</tr>");
+          gameName = "<td>" + game.name + "</td>";
+          gameBundle = "<td>" + game.month + " " + game.year + "</td>";
+          gameId = "<td>" + game.steam_id + "</td>";
 
-      });
+          $("#humble-list tbody").append("<tr>" + gameName + gameBundle + gameId + "</tr>");
+
+        })
+      })
+    // })
+
+  .done(function() {
+    console.log("after");
+    $("#humble-list").searchable({
+      searchField : "#search",
+      // searchType  : "fuzzy",
+      clearOnLoad : true
     });
-  });
+  })
+
+  // ).done(function() {
+  //   console.log("Then go!");
+  //   $("#humble-list").searchable({
+  //     searchField : "#search",
+  //     searchType  : "fuzzy",
+  //     clearOnLoad : true
+  //   });
+  // });
+
+  // setTimeout(function() {
+
+    // console.log("Timeout go!");
+    // $("#humble-list").searchable({
+    //   searchField : "#search",
+    //   searchType  : "fuzzy",
+    //   clearOnLoad : true
+    // });
+
+  // }, 5000);
 });
